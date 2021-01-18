4493ae12abe842efb96bc92b7fe608065a91def4
Kenta-came <65546166+Kenta-came@users.noreply.github.com>
2021-01-18 21:02:46 +0900

    deviseの導入

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+    validates :email, format: { with: /[@]/ , message: 'メールアドレスに「＠」を挿入してください！'}
+
+    with_options format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i ,message: '半角英数字を混合してください！'} do
+      validates :password
+    end
+  end
+end
